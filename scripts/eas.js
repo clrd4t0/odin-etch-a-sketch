9a3670ae18029dff4f7519ddf11fa59935d586cc
Ivan Lai <32264108+clrd4t0@users.noreply.github.com>
2024-05-12 15:35:10 +0800

    feat: Complete basic assignment with no styling

---
+++ scripts/eas.js
@@ -0,0 +1,35 @@
+// All the querySelectors goes here
+container = document.querySelector("#container");
+createDivsButton = document.querySelector("#create-divs-btn");
+
+// All eventListners goes here
+createDivsButton.addEventListener("click", () => createCanvas());
+
+function createCanvas() {
+    let dimension;
+    getInput:
+    while(true) {
+        dimension = prompt("Dimension?", "16");
+        if (dimension !== NaN && dimension > 0 && dimension <= 100) {
+            break getInput;
+        } else {
+            alert("Please input a number between 1 and 100.");
+        }
+    }
+    container.textContent = "";
+    let size = 70 / dimension;
+    columnDimension = `width: ${size}vh`;
+    rowDimension = `height: ${size}vh`;
+    for (let j = 0; j < dimension; j++) {
+        row = document.createElement("div");
+        row.classList.add("row");
+        row.style.cssText = rowDimension;
+        for (let i = 0; i < dimension; i++) {
+            column = document.createElement("div");
+            column.classList.add("column");
+            column.style.cssText = columnDimension;
+            row.appendChild(column);
+        }
+        container.appendChild(row);
+    }
+}
